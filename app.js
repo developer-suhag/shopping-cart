820bd5da0d029cba159d02ad3f771a6862a183fe
developer-suhag <developersuhag@gmail.com>
2021-08-18 00:47:34 +0600

    increase and decrease

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
 // increase quantity
-function inputIncrease(inputId) {
+function updateQuantity(inputId, isPlus, total, isMobile) {
     const inputField = document.getElementById(inputId);
-    const inputQuantity = inputField.value;
-    return inputField.value = parseInt(inputQuantity) + 1;
+    let inputQuantity = inputField.value;
+    if (isPlus == true) {
+        inputQuantity = parseInt(inputQuantity) + 1;
+    } else if (inputQuantity > 0) {
+        inputQuantity = parseInt(inputQuantity) - 1;
+    }
+    inputField.value = inputQuantity;
 
-}
-// case
+    if (isMobile) {
+        const totalPrice = document.getElementById(total)
+        totalPrice.innerText = inputQuantity * 1219;
+    } else {
+        const totalPrice = document.getElementById(total)
+        totalPrice.innerText = inputQuantity * 59;
+    }
+
+};
+
+// decrease quantity
+// function inputDecrease(inputId) {
+//     const inputField = document.getElementById(inputId);
+//     const inputQuantity = inputField.value;
+//     if (inputQuantity > 0) {
+//         return inputField.value = parseInt(inputQuantity) - 1;
+//     }
+
+// }
+
+// Handle Mobile increase and decrease events 
+// increase 
+document.getElementById('mobile-plus').addEventListener('click', function () {
+    updateQuantity('mobile-number', true, 'mobile-total', true);
+});
+// decrease 
+document.getElementById('mobile-minus').addEventListener('click', function () {
+    updateQuantity('mobile-number', false, 'mobile-total', true);
+});
+
+// handle case increase and decrease events
+// increase
 document.getElementById('case-plus').addEventListener('click', function () {
     // const caseInput = document.getElementById('case-number');
     // const caseQuantity = caseInput.value;
     // caseInput.value = parseInt(caseQuantity) + 1;
-    inputIncrease('case-number')
+    updateQuantity('case-number', true, 'case-total', false)
 });
-
-// Mobile 
-document.getElementById('mobile-plus').addEventListener('click', function () {
-    inputIncrease('mobile-number')
+// decrease 
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateQuantity('case-number', false, 'case-total', false);
 })
